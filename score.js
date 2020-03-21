4429bb6e10cbb7aaa506abab2f3cb991ab9d9863
Salman Zafar <salman.x916@gmail.com>
2020-03-21 17:28:07 +0530

    Added ScoreKeeper App

---
+++ score.js
@@ -0,0 +1,56 @@
+var p1Button = document.querySelector("#p1");
+var resetButton = document.querySelector("#reset");
+var p1Display = document.querySelector("#p1Display");
+var numInput = document.querySelector("input[type='number']");
+var playingToDisplay = document.querySelector("p span");
+
+
+var p1Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+var p2Button = document.querySelector("#p2");
+var p2Display = document.querySelector("#p2Display");
+var p2Score = 0;
+
+p1Button.addEventListener("click", function () {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === winningScore) {
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener("click", function () {
+    if (!gameOver) {
+        p2Score++;
+        if (p2Score === winningScore) {
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+});
+
+resetButton.addEventListener("click", function () {
+    reset();
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+numInput.addEventListener("change", function () {
+    playingToDisplay.textContent = numInput.value;        //We can use *this* in place of
+    winningScore = Number(numInput.value);               //numInput as it was refering to
+    reset();                                            //whichever the event was listening on.
+});
